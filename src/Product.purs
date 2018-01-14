cbfd395910b5a34229cb0b21792d908d2b92ea01
Steven Troxler <steven.troxler@gmail.com>
2018-01-14 12:05:52 -0800

    Add an Eff call to our example
    I extended the Eff slightly from the video to include a return
    value, since in general we might care about value-producing
    effects when integrating purescript with javascript.

--- src/Product.purs
+++ src/Product.purs
@@ -1,8 +1,18 @@
-module Product (format, formatJs) where
+module Product (format, formatJs, logPrice) where
 
-import Prelude (map, show, ($), (<>))
-import Data.Maybe
+-- note: the discard is needed for using do notation
+import Prelude (map, show, ($), (<>), discard, pure, (+))
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff (Eff)
+import Data.Maybe (Maybe, maybe)
 import Data.Function.Uncurried (Fn3, mkFn3)
+
+
+logPrice :: forall e. Number -> Eff (console :: CONSOLE | e) String
+logPrice price = do
+  log $ "The price coming in is " <> show price
+  pure $ "If we were to increment the price it would be " <> show (price + 1.0)
+
 
 formatJs :: Fn3 String Number (Maybe Int) String
 formatJs = mkFn3 format
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { formatJs } from '../purs/Product'
+import { formatJs, logPrice } from '../purs/Product'
 import { Nothing, Just } from '../purs/Data.Maybe'
 
 const Product = ({ price, quantity, title }) => {
+  let incrementedString = logPrice(price)()
+  console.log("The output of our Eff call was: ", incrementedString)
   let q = quantity ? Just.create(quantity) : Nothing.value
   return <div>
            { formatJs(title, price, q) }
