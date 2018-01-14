759834d8b97ac295a76f75a241e5f9053ac0797c
Steven Troxler <steven.troxler@gmail.com>
2018-01-14 11:24:24 -0800

    Use Data.Function.Uncurried to export to js
    Converted the purescript `format` function into an uncurried
    Fn3. Note that we don't have to wrap the args in a tuple or anything;
    purescript exposes it as a regular javascript function.

--- src/Product.purs
+++ src/Product.purs
@@ -1,7 +1,11 @@
-module Product (format) where
+module Product (format, formatJs) where
 
 import Prelude (map, show, ($), (<>))
 import Data.Maybe
+import Data.Function.Uncurried (Fn3, mkFn3)
+
+formatJs :: Fn3 String Number (Maybe Int) String
+formatJs = mkFn3 format
 
 format :: String -> Number -> Maybe Int -> String
 format title price quantity =
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { format } from '../purs/Product'
+import { formatJs } from '../purs/Product'
 import { Nothing, Just } from '../purs/Data.Maybe'
 
 const Product = ({ price, quantity, title }) => {
   let q = quantity ? Just.create(quantity) : Nothing.value
   return <div>
-           { format(title)(price)(q) }
+           { formatJs(title, price, q) }
          </div>
 }
 
